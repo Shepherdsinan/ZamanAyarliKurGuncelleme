880708cba0b3aeda85858f666ec786a123e811b6
Shepherdsinan <65864888+Shepherdsinan@users.noreply.github.com>
2022-10-16 02:39:31 +0300

    Project Created

---
+++ CrmposZamanlıKurGüncelleme/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CrmposZamanlıKurGüncelleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Periyot2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.Periyot2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 97);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(525, 522);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Son işlem Zamanı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(314, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "--:--";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Saat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dakika";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Dakika";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Saat";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 33);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(64, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(59, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 56);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Periyot1";
+            // 
+            // Periyot2
+            // 
+            this.Periyot2.Controls.Add(this.textBox4);
+            this.Periyot2.Controls.Add(this.textBox3);
+            this.Periyot2.Controls.Add(this.label5);
+            this.Periyot2.Controls.Add(this.label6);
+            this.Periyot2.Location = new System.Drawing.Point(314, 12);
+            this.Periyot2.Name = "Periyot2";
+            this.Periyot2.Size = new System.Drawing.Size(167, 57);
+            this.Periyot2.TabIndex = 13;
+            this.Periyot2.TabStop = false;
+            this.Periyot2.Text = "Periyot2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 623);
+            this.Controls.Add(this.Periyot2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Crmposzamanlıkur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Periyot2.ResumeLayout(false);
+            this.Periyot2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox Periyot2;
+    }
+}
+
---
+++ CrmposZamanlıKurGüncelleme/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CrmposZamanlıKurGüncelleme
+{
+    public partial class Form1 : Form
+    {
+        MRTREntities db = new MRTREntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dt = new DataTable();        
+        DataRow dr;
+        public DateTime ActualCurrencyDate { get; set; }
+        string url;
+        XmlTextReader rdr;
+        XmlDocument XmlDoc;
+
+        
+
+        private void GenerateApiUrl()
+        {
+            url = string.Empty;
+                       
+            url = "http://www.tcmb.gov.tr/kurlar/today.xml";
+           
+            rdr = new XmlTextReader(url);
+        }
+
+        public DataTable source()
+        {
+            dt.Rows.Clear();
+            dt.Columns.Clear();
+            try
+            {
+                dt.Columns.Add(new DataColumn("Sıra", typeof(int)));
+                dt.Columns.Add(new DataColumn("Tarih", typeof(DateTime)));
+                dt.Columns.Add(new DataColumn("Kod", typeof(string)));
+                dt.Columns.Add(new DataColumn("Döviz_alış", typeof(decimal)));
+                dt.Columns.Add(new DataColumn("Döviz_satış", typeof(decimal)));
+
+                
+
+                for (int attempts = 0; attempts <= 5; attempts++)
+                {
+                    try
+                    {
+                        GenerateApiUrl();
+                        XmlDoc = new XmlDocument();
+                        XmlDoc.Load(rdr);
+
+                        break;
+                    }
+                    catch (WebException ex)
+                    {
+                        if (ex.Response != null)
+                        {
+                            // 404 not found
+                            HttpWebResponse errorResponse = ex.Response as HttpWebResponse;
+                            if (errorResponse.StatusCode == HttpStatusCode.NotFound)
+                            {
+                                // bir gün öncesi kontrol edilir
+                                ActualCurrencyDate = ActualCurrencyDate.AddDays(-1);
+                            }
+                            else
+                            {
+                                throw new Exception("Kur bilgisi bulunamadı.");
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("Kur bilgisi bulunamadı.");
+                        }
+                    }
+                }
+
+                // Load metodu ile xml yüklüyoruz
+                XmlNode tarih = XmlDoc.SelectSingleNode("/Tarih_Date/@Tarih");
+                XmlNodeList mylist = XmlDoc.SelectNodes("/Tarih_Date/Currency");
+                XmlNodeList adi = XmlDoc.SelectNodes("/Tarih_Date/Currency/Isim");
+                XmlNodeList kod = XmlDoc.SelectNodes("/Tarih_Date/Currency/@Kod");
+                XmlNodeList doviz_alis = XmlDoc.SelectNodes("/Tarih_Date/Currency/ForexBuying");
+                XmlNodeList doviz_satis = XmlDoc.SelectNodes("/Tarih_Date/Currency/ForexSelling");
+                XmlNodeList efektif_alis = XmlDoc.SelectNodes("/Tarih_Date/Currency/BanknoteBuying");
+                XmlNodeList efektif_satis = XmlDoc.SelectNodes("/Tarih_Date/Currency/BanknoteSelling");
+
+                
+                int x = 17;
+               
+                
+                for (int i = 0; i < x; i++)
+                {
+                    dr = dt.NewRow();
+                    dr[0] = (i + 1);
+                    dr[1] = DateTime.Now.Date; //.ToString("dd.MM.yyyy");
+                    
+                    dr[2] = kod.Item(i).InnerText.ToString();
+                    // Kod satırları
+                    dr[3] = decimal.Parse(doviz_alis.Item(i).InnerText, CultureInfo.InvariantCulture);
+                    // Döviz Alış
+                    dr[4] = decimal.Parse(doviz_satis.Item(i).InnerText, CultureInfo.InvariantCulture);
+                    // Döviz  Satış
+                    //dr[5] = efektif_alis.Item(i).InnerText.ToString();
+                    //// Efektif Alış
+                    //dr[6] = efektif_satis.Item(i).InnerText.ToString();
+                    // Efektif Satış.
+                    dt.Rows.Add(dr);
+                }
+            }
+            catch (Exception expc)
+            {
+                MessageBox.Show(expc.ToString(), "İnfo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+                     
+            return dt;
+            // DataTable ı döndürüyoruz.
+        }
+        int dvzid;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string saat, dakika,saat2,dakika2,saniye;
+            saat = textBox1.Text;
+            dakika = textBox2.Text;
+            saat2 = textBox3.Text;
+            dakika2 = textBox4.Text;
+            saniye = "1";
+
+
+            if ((Convert.ToString(DateTime.Now.Hour)) == saat && (Convert.ToString(DateTime.Now.Minute) == dakika) && (Convert.ToString(DateTime.Now.Second)) == saniye)
+            {
+                sync();
+            }
+
+            if ((Convert.ToString(DateTime.Now.Hour)) == saat2 && (Convert.ToString(DateTime.Now.Minute) == dakika2) && (Convert.ToString(DateTime.Now.Second)) == saniye)
+            {
+                sync();
+            }
+        }
+
+        private void sync()
+        {
+            dataGridView1.DataSource = source();
+            CultureInfo culture = new CultureInfo("en-US", true);
+
+            // en-US kültüründe ondalık ayracı zaten "." olduğu için değerlendirmeye gerek yok.          
+
+
+            for (int i = 0; i < source().Rows.Count; i++)
+            {
+                DataRow dr = source().Rows[i];
+                switch (dr["Kod"].ToString())
+                {
+                    case "USD":
+                        dvzid = 2;
+                        break;
+                    case "EUR":
+                        dvzid = 3;
+                        break;
+                    case "AUD":
+                        dvzid = 4;
+                        break;
+                    case "DKK":
+                        dvzid = 5;
+                        break;
+                    case "GBP":
+                        dvzid = 6;
+                        break;
+                    case "CHF":
+                        dvzid = 7;
+                        break;
+                    case "SEK":
+                        dvzid = 8;
+                        break;
+                    case "CAD":
+                        dvzid = 9;
+                        break;
+                    case "KWD":
+                        dvzid = 10;
+                        break;
+                    case "NOK":
+                        dvzid = 11;
+                        break;
+                    case "SAR":
+                        dvzid = 12;
+                        break;
+                    case "JPY":
+                        dvzid = 13;
+                        break;
+                    case "BGN":
+                        dvzid = 14;
+                        break;
+                    case "RUB":
+                        dvzid = 15;
+                        break;
+                    case "IRR":
+                        dvzid = 16;
+                        break;
+                    case "CNY":
+                        dvzid = 17;
+                        break;
+                    case "RON":
+                        dvzid = 18;
+                        break;
+                    default:
+                        dvzid = 0;
+                        break;
+                }
+
+
+                var dcalis = dr.Field<decimal>("Döviz_alış");
+                var dcsatis = dr.Field<decimal>("Döviz_satış");
+                var tatarih = DateTime.Now.Date;
+                int count = db.TCMB_KUR_CRMPOS.Count(a => a.TARIH == tatarih);
+
+                if (count > 0)
+                {
+                    var x = db.TCMB_KUR_CRMPOS.Where(a => a.DOVIZ_AD == dvzid && a.TARIH == tatarih).First();
+                    x.TARIH = tatarih;
+                    x.ALIS = dcalis;
+                    x.SATIS = dcsatis;
+                    x.DOVIZ_AD = dvzid;
+                    x.SIRA = dr.Field<int>("Sıra");
+                }
+                else
+                {
+                    db.TCMB_KUR_CRMPOS.Add(new TCMB_KUR_CRMPOS()
+                    { //add data to class objects variable
+                        TARIH = dr.Field<DateTime>("Tarih"),
+                        ALIS = dcalis,
+                        SATIS = dcsatis,
+                        DOVIZ_AD = dvzid,
+                        SIRA = dr.Field<int>("Sıra")
+                    });
+                }
+
+
+            }
+            db.SaveChanges();
+            label2.Text = DateTime.Now + "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            button1.Enabled = false;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
